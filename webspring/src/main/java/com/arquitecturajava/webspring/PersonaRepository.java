9e262bab7e3f42821287e2e94f51f93fcd4480d7
Jorge Salan Perez <jorge.salan@enverus.com>
2025-02-12 15:47:24 +0100

    Inicio

---
+++ webspring/src/main/java/com/arquitecturajava/webspring/PersonaRepository.java
@@ -0,0 +1,27 @@
+package com.arquitecturajava.webspring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonaRepository {
+
+	private List<Persona> personas = new ArrayList<Persona>();
+	
+	public PersonaRepository() {
+		personas.add(new Persona("Juan"));
+		personas.add(new Persona("Pedro"));
+		personas.add(new Persona("Luis"));
+	}
+	
+	public void addPersona(Persona persona) {
+		personas.add(persona);
+	}
+	
+	public List<Persona> getPersonas() {
+        return personas;
+    }
+	
+}
---
+++ webspring/src/main/java/com/arquitecturajava/webspring/PersonaService.java
@@ -0,0 +1,23 @@
+package com.arquitecturajava.webspring;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService {
+
+	@Autowired
+	private PersonaRepository personaRepository;
+
+	public void addPersona(Persona persona) {
+		personaRepository.addPersona(persona);
+	}
+
+	public List<Persona> getPersonas() {
+		return personaRepository.getPersonas();
+	}
+
+	
+}
