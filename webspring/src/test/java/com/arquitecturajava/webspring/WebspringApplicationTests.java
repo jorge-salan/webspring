581d7d22318db185ac76852bc47b1b73fde0bba4
Jorge Salan Perez <jorge.salan@enverus.com>
2025-02-17 16:24:14 +0100

    Add unit test

--- webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
+++ webspring/src/test/java/com/arquitecturajava/webspring/WebspringApplicationTests.java
@@ -1,10 +1,25 @@
 package com.arquitecturajava.webspring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.arquitecturajava.webspring.models.Persona;
 
 @SpringBootTest
 class WebspringApplicationTests {
+
+	@Autowired
+	PersonaRepository repository;
+
+	@Test
+	void addPersonaRepositoryTest() {
+		Persona p = new Persona("Pepe");
+		repository.addPersona(p);
+		assertEquals(4, repository.getPersonas().size());
+	}
 
 	@Test
 	void contextLoads() {
